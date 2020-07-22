4e413234c29dc3e40f8d952811f48e1e3cb08cf3
Afique <afique.dev@gmail.com>
2020-07-23 03:12:08 +0600

    basic todo-list initialized

---
+++ app.js
@@ -0,0 +1,35 @@
+// All Query Selectors
+const list = document.querySelector(".todo-list");
+const addForm = document.querySelector(".add");
+const endMsg = document.querySelector(".endMsg");
+
+// Populate todo list function
+function populateList(todo) {
+  list.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
+                <span>${todo}</span>
+                <i class="far fa-trash-alt delete"></i>
+            </li>`;
+}
+
+// Remove a todo function
+function removeTodo(target) {
+  target.parentElement.remove();
+}
+
+// Adding a todo event
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+  if (addForm.add.value.trim().length > 0) {
+    populateList(todo);
+    addForm.reset();
+  }
+});
+
+// Removing a todo event
+list.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("delete")) {
+    removeTodo(e.target);
+  }
+});
